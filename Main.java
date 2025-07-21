d4f4a3214f73469a15cec01d31d4993f96b8e7a1
Maram-Alabri <maramalabri22@gmail.com>
2025-07-21 19:25:36 +0400

    Step6

--- Main.java
+++ Main.java
@@ -96,4 +96,24 @@ public class MiniBankSystem {
             System.out.println("Withdrew $" + amount + ". New Balance: $" + balance);
         }
     }
+    public class SavingsAccount extends BankAccount {
+        private double interestRate;
+
+        public SavingsAccount(String accountName, String accountNumber, double balance, double interestRate) {
+            super(accountName, accountNumber, balance);
+            this.interestRate = interestRate;
+        }
+
+        public void applyInterest() {
+            double interest = balance * interestRate / 100;
+            balance += interest;
+            System.out.println("Applied interest: $" + interest + ". New balance: $" + balance);
+        }
+
+        @Override
+        public void showAccountInfo() {
+            super.showAccountInfo();
+            System.out.println("Interest Rate: " + interestRate + "%");
+        }
+    }
 }
